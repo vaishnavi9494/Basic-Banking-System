80be652975dda83fe7eea595cb88e0a50bfd2364
vaishnavi9494 <vivi.ak2012@gmail.com>
2021-10-21 03:38:18 +0530

    twelvth

--- model/conn.js
+++ model/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/bankingSystem", {useNewUrlParser : true}).then(() => {
+mongoose.connect("mongodb://localhost:27017/bankingSystem"||process.env.MONGO_URI, {useNewUrlParser : true}).then(() => {
     console.log("connection successful");
 }).catch((e) => {
     console.log(e);
