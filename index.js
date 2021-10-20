71d82b244fb564afe358aa04e673100ea5e73c7b
vaishnavi9494 <vivi.ak2012@gmail.com>
2021-10-21 02:59:36 +0530

    seventh

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path")
 require("./model/conn")
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
 app.use(express.json());
