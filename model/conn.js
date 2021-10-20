bb5166209d9b45c22ed8ae5762eb9e62f634e82c
vaishnavi9494 <vivi.ak2012@gmail.com>
2021-10-21 03:55:16 +0530

    fifteen

--- model/conn.js
+++ model/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/bankingSystem"||process.env.MONGO_URI, {useNewUrlParser : true}).then(() => {
+mongoose.connect("mongodb://localhost:27017/bankingSystem" || "mongodb+srv://vrocks123:NI8gUeTuZTxuIlJh@cluster01.cskrp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser : true, useNewUrlParser: true, useFindAndModify: false}).then(() => {
     console.log("connection successful");
 }).catch((e) => {
     console.log(e);
