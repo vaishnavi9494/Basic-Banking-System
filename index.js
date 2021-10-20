6ca2a90a3bc3efaee9482fb3413b09e1830665b4
vaishnavi9494 <vivi.ak2012@gmail.com>
2021-10-21 03:45:11 +0530

    fourteen

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("./model/conn")
 const app = express();
 const port = process.env.PORT || 5000;
 
-console.log(process.env.MONGO_URI);
+console.log("yoyo", process.env.MONGO_URI);
 
 app.set('view engine', 'ejs');
 app.use(express.json());
